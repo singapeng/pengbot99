d1746459a6e2d0d5c7e5380120da63c25d1870d3
Olivier Thibaut <olivier@singapenguin.net>
2024-04-05 22:24:03 -0700

    + Added Miniprix schedule support for Mirroring

--- py/events.py
+++ py/events.py
@@ -5,7 +5,7 @@ class Event(object):
     def __init__(self, name, cycle=0, cycle_minute=0, start_minute=0, end_minute=0, rotation=None, rotation_offset=0):
         super().__init__()
         # the event's internal name
-        self.name = name
+        self._name = name
         # what is the event's cycle number
         self.cycle = cycle
         # what minute of the cycle the event was created in
@@ -20,6 +20,12 @@ class Event(object):
         self.rotation_offset = rotation_offset
         # start time
         self.start_time = None
+
+    @property
+    def name(self):
+        """ The Event's human-readable name
+        """
+        return self._name
 
     @property
     def duration(self):
@@ -45,3 +51,41 @@ class Event(object):
         """
         start_str = self.start_time.strftime("%Y-%m-%d %H:%M")
         return "{0} - {1} ({2} minutes)".format(start_str, self.name, self.duration)
+
+
+class MiniPrixEvent(Event):
+    def __init__(self, mp_type, mp_id, race1, race2, race3, start_minute=0, end_minute=0, mirrored="000"):
+        if mp_type == "classicprix":
+            code = "ClassicMiniPrix"
+        else:
+            code = "MiniPrix"
+        miniprix_id = "{:s}{:s}".format(code, mp_id)
+        name = "{0} > {1} > {2} ({3})".format(race1, race2, race3, miniprix_id)
+        super().__init__(name=miniprix_id, start_minute=start_minute, end_minute=end_minute)
+        self._race1 = race1
+        self._race2 = race2
+        self._race3 = race3
+        self._mirrored = mirrored
+
+    @property
+    def name(self):
+        name_fmt = "{0} > {1} > {2} ({3})"
+        return name_fmt.format(self.race1, self.race2, self.race3, self._name)
+
+    @property
+    def race1(self):
+        if self._mirrored[0] == '1':
+            return 'm' + self._race1
+        return self._race1
+
+    @property
+    def race2(self):
+        if self._mirrored[1] == '1':
+            return 'm' + self._race2
+        return self._race2
+
+    @property
+    def race3(self):
+        if self._mirrored[2] == '1':
+            return 'm' + self._race3
+        return self._race3
--- py/miniprix.py
+++ py/miniprix.py
@@ -13,6 +13,7 @@ MP_CYCLES = 10
 # prix 003.
 CLASSIC_LINE_UP_OFFSET = 14
 MINIPRIX_LINE_UP_OFFSET = 0
+MIRROR_LINE_UP_OFFSET = 6
 
 
 def print_miniprix_rows(rows):
@@ -23,34 +24,74 @@ def print_miniprix_rows(rows):
         minute += 1
 
 
+def _trim_schedule(sched):
+    if sched[-1][1] == 'next':
+        return sched[:-1]
+    else:
+        return sched
+
+
 class MiniPrixManager(object):
     """
     """
-    def __init__(self, event_name, cycle_manager, mp_schedule):
+    def __init__(self, event_name, cycle_manager, mp_schedule, mirror=None):
         super().__init__()
         self.name = event_name
         self.mgr = cycle_manager
-        # we don't actually care about the 'next' entry here
-        if mp_schedule[-1][1] == 'next':
-            self.schedule = mp_schedule[:-1]
-        else:
-            self.schedule = mp_schedule
+        self._mp_schedule = mp_schedule
+        self._mirror_schedule = mirror
+        self.mirror_lineup_offset = MIRROR_LINE_UP_OFFSET
         self.mp_cycles = MP_CYCLES
         if self.name == "classicprix":
             self.lineup_offset = CLASSIC_LINE_UP_OFFSET
         else:
             self.lineup_offset = MINIPRIX_LINE_UP_OFFSET
 
+    @property
+    def schedule(self):
+        return _trim_schedule(self._mp_schedule)
+
+    @property
+    def mirror_schedule(self):
+        if not self._mirror_schedule:
+            return None
+        return _trim_schedule(self._mirror_schedule)
+
     def _get_start_mp_row(self, cycle):
         """ 
         """
         return (cycle * self.mp_cycles - self.lineup_offset) % len(self.schedule)
 
+    def _get_start_mirror_row(self, cycle):
+        """ 
+        """
+        return (cycle * self.mp_cycles - self.mirror_lineup_offset) % len(self.mirror_schedule)
+
+    def _build_rows_from_rotation(self, get_start_fn, sched, cycle):
+        first_row = get_start_fn(cycle)
+        last_row = first_row + self.mp_cycles
+        if last_row > len(sched):
+            # Because the mirror schedule length is 9 and there are 10 prix
+            # rows, it seems possible that doubling the table size may not
+            # be enough in edge cases so let's triple it.
+            rows = (sched * 3)[first_row:last_row]
+        else:
+            rows = sched[first_row:last_row]
+        return rows
+
+    def _get_track_selection_rows(self, cycle):
+        return self._build_rows_from_rotation(self._get_start_mp_row, self.schedule, cycle)
+
+    def _get_mirroring_rows(self, cycle):
+        if self._mirror_schedule:
+            return self._build_rows_from_rotation(self._get_start_mirror_row, self.mirror_schedule, cycle)
+        return [(0, "000")] * self.mp_cycles
+
     def get_miniprix(self, timestamp=None):
-        next_cmp = self.mgr.when_event(names=[self.name], timestamp=timestamp)
-        if not next_cmp:
+        next_mp = self.mgr.when_event(names=[self.name], timestamp=timestamp)
+        if not next_mp:
             return None
-        cycle = next_cmp[0].cycle // (len(next_cmp[0].rotation) or 1)
+        cycle = next_mp[0].cycle // (len(next_mp[0].rotation) or 1)
         start_time = None
         if not timestamp:
             current = self.mgr.get_current_event()
@@ -66,25 +107,20 @@ class MiniPrixManager(object):
                 cycle -= 1
                 start_time = ts_evt.start_time
         if not start_time:
-            start_time = next_cmp[0].start_time
+            start_time = next_mp[0].start_time
 
-        first_row = self._get_start_mp_row(cycle)
-        last_row = first_row + 10
-        if last_row > len(self.schedule):
-            rows = (self.schedule + self.schedule)[first_row:last_row]
-        else:
-            rows = self.schedule[first_row:last_row]
-        return self.eventify_rows(start_time, rows)
+        rows = self._get_track_selection_rows(cycle)
+        mirror_rows = self._get_mirroring_rows(cycle)
+        return self.eventify_rows(start_time, rows, mirror_rows)
 
-    def eventify_rows(self, start_time, rows):
+    def eventify_rows(self, start_time, rows, mirror_rows):
         res = []
-        if self.name == "classicprix":
-            code = "ClassicMiniPrix"
-        else:
-            code = "MiniPrix"
         for idx, row in enumerate(rows):
-            name = "{:s} ({:s}{:03d})".format(row[1], code, row[0])
-            evt = events.Event(name, start_minute=idx, end_minute=idx + 1)
+            name = self.name
+            mpid = "{:03d}.{:s}".format(int(row[0]), str(mirror_rows[idx][0]))
+            mirror = mirror_rows[idx][1]
+            r1, r2, r3 = row[1].split(' > ')
+            evt = events.MiniPrixEvent(name, mpid, r1, r2, r3, start_minute=idx, end_minute=idx + 1, mirrored=mirror)
             evt.set_start_time(start_time + timedelta(minutes=idx))
             res.append(evt)
         return res
