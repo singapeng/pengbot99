c2131184af9c33f63682112c98270044cab08fe2
Olivier <olivier@singapenguin.net>
2024-06-09 20:54:39 -0700

    - fix bug in thread unarchiving where the first instance of updating after unarchive fails because of incorrect None function return

--- py/apiadapter.py
+++ py/apiadapter.py
@@ -16,8 +16,7 @@ async def get_thread(channel, thread_id):
             if thread.id == thread_id:
                 await thread.unarchive()
                 utils.log("Successfully unarchived thread {0}.".format(thread.id))
-                counter = -1
-                break
+                return thread
         if counter >= 0:
             utils.log("Did not find thread amongst {0} in archive.".format(counter))
     # This will probably trigger an attribute error downstream (uncaught).
